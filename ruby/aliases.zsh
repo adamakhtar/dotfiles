9ef45bdc5fd81e143925f338d8f9a67d04ff67e1
Adam Akhtar <adam.akhtar@gmail.com>
2017-10-28 15:25:01 +0900

    add more aliases for working with rails and postgres

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -3,9 +3,19 @@ alias be='bundle exec'
 alias br='bin/rake'
 alias brs='bin/rspec'
 alias brc='bin/rails console'
+alias brgmig='bin/rails generate migration'
+alias brgc='bin/rails generate controller'
+alias brgm='bin/rails generate model'
+alias bss='bin/spring stop'
 alias brdp='bin/rake dev:prime'
+alias brdbr='bin/rake db:rollback'
+alias powr='powder restart'
+alias pows='powder stop'
 alias migrate="bundle exec rake db:migrate db:rollback && bundle exec rake db:migrate"
 alias migrate_test='bundle exec rake db:migrate RAILS_ENV=test'
+alias pgr='brew services restart postgres'
+alias pgs='brew services stop postgres'
+alias pgl='cat /usr/local/var/postgres/server.log'
 
 # Kill rails server process e.g. `krs 3000`
 function krs() { kill -9 $(lsof -i tcp:$1 -t) }
